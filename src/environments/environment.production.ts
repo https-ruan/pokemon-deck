cebd2411ddbb6be5399bfcae90060ca92148bb4b
Ruan Araújo <ruands2002@gmail.com>
2024-04-30 00:58:36 -0300

    feat(environment): adicionado envs e interface

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,8 @@
+import { IEnvironment } from '@core/interfaces/environment.interface';
+
+export const environment: IEnvironment = {
+  production: true,
+  api: {
+    url: 'https://api.pokemontcg.io/v2',
+  },
+};
